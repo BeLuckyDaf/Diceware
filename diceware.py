8b8a1c0a4530ee6a01542c7e621ed85105c3f2df
Vladislav Smirnov <beluckydaf@gmail.com>
2017-12-03 13:05:24 +0300

    Initial commit

--- diceware.py
+++ diceware.py
@@ -7,44 +7,44 @@ request_id = 78342  # some number
 
 def generate(api):
 	# getting a dictionary from a local directory
-    dictionary = os.path.join(os.path.dirname(os.path.realpath(__file__)), "dictionary.asc")
+	dictionary = os.path.join(os.path.dirname(os.path.realpath(__file__)), "dictionary.asc")
 	
 	# a request to send
-    payload = {"jsonrpc": "2.0", "method": "generateIntegers",
-               "params": {"apiKey": api, "n": "30", "min": "1", "max": "6"}, "id": request_id}
+	payload = {"jsonrpc": "2.0", "method": "generateIntegers",
+				"params": {"apiKey": api, "n": "30", "min": "1", "max": "6"}, "id": request_id}
     
 	# random.org url for json-rpc requests
 	url = "https://api.random.org/json-rpc/1/invoke"
 	
 	# specifying a header
-    headers = {'content-type': 'application/json-rpc'}
+	headers = {'content-type': 'application/json-rpc'}
 	
 	# receiving the response from random.org (numbers from 1 to 6)
-    response = requests.post(url, data=json.dumps(payload), headers=headers)
+	response = requests.post(url, data=json.dumps(payload), headers=headers)
 	
 	# parsing the data
-    random = json.loads(response.text)['result']['random']['data']
+	random = json.loads(response.text)['result']['random']['data']
 
 	# temporary variables
-    temp = ""
-    pairs = []
-    password = ""
+	temp = ""
+	pairs = []
+	password = ""
 
     
 	# splitting numbers into pairs
 	for i in range(0, len(random)):
-        temp += str(random[i])
-        if i % 5 == 4:
-            pairs.append(temp)
-            temp = ""
+		temp += str(random[i])
+		if i % 5 == 4:
+			pairs.append(temp)
+			temp = ""
 			
 	# making words using the dictionary and the pairs above
-    for combination in pairs:
-        with open(dictionary, 'r') as sfile:
-            for line in sfile:
-                if combination in line:
-                    password += (line[6:-1]) + " "
+	for combination in pairs:
+		with open(dictionary, 'r') as sfile:
+			for line in sfile:
+				if combination in line:
+					password += (line[6:-1]) + " "
 
 					
 	# returns a string of format "word word word word word word"
-    return password[:-1]
+	return password[:-1]
